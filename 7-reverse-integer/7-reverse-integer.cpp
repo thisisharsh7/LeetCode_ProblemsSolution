bf70acd069cb1ccca47f2de0a8bf303fe5aad273
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-04-30 21:53:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (34.07%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int r=0;//reversed no. storing
+        int l;//left no.
+        while(x!=0){
+            l=x%10;
+            x/=10;
+           
+            if(r > INT_MAX/10 || r==INT_MAX && l>7){
+                return 0;
+            }if(r < INT_MIN/10 || r==INT_MIN && l<-8){
+                return 0;
+            }
+             r=(r*10) + l;
+            
+        }
+        
+        return r;
+    }
+};
