6775492113504a26767c6dcbb227d3c232d5ee63
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-05-01 21:58:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.99%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -6,13 +6,13 @@ public:
         while(x!=0){
             l=x%10;
             x/=10;
-           
             if(r > INT_MAX/10 || r==INT_MAX && l>7){
                 return 0;
             }if(r < INT_MIN/10 || r==INT_MIN && l<-8){
                 return 0;
             }
-             r=(r*10) + l;
+            r=(r*10) + l;
+
             
         }
         
