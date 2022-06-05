a4c0680eedd39bd8953ea7b784d005af92adea59
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-06-05 06:00:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (86.43%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
-    void findCombinations(int idx,int target,int k,vector<int> &sans,vector<vector<int>> &ans){
+    void findCombinations(int idx,int target,int k,vector<int> &sans,vector<vector<int>> &ans){//passing by refrence so that 2D vector can have it's value stored at every function call
         if(target==0 and sans.size()==k){
-            ans.push_back(sans);
+            ans.push_back(sans);//pushing the 1D vector with the sum equal to target
         }
         for(int i=idx;i<10;i++){
-            if(i>target ){
+            if(i>target ){//at every i greater than target we breaks the loop
                 break;
             }
 
-                sans.push_back(i);
-            findCombinations(i+1,target-i,k,sans,ans);
-            sans.pop_back();
+            sans.push_back(i);//pushing i which are less than target
+            findCombinations(i+1,target-i,k,sans,ans);//recursive call with index=i+1 i.e 1 increased at every call so that no two same elements are together
+            sans.pop_back();//on going back we pop all the elements of 1D vector
         }
     }
     
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> rev;
-        vector<int> sans;
-        findCombinations(1,n,k,sans,rev);
+        vector<vector<int>> rev;//return type
+        vector<int> sans;//assumption of single vector
+        findCombinations(1,n,k,sans,rev);//this function is used to give the desired output in our 2D vector by recursion
         return rev;
     }
 };
-// class Solution {
-// public:
-//     vector<vector<int>> combinationSum3(int k, int n) 
-//     {
-//         vector<vector<int>>ans;
-//         vector<int>comb;
-//         findcomb(n, ans, comb, 1,k);
-//         return ans;        
-//     }
-//     void findcomb(int target,   vector<vector<int>>&ans,vector<int>& combination,int begin,int need)
-//     {
-//         if (need< combination.size() or target<0)
-//             return;
-        
-//         if (target==0 and combination.size()==need)
-//         { 
-//             ans.push_back(combination);
-//             return ;
-//         }        
-//         for(int i=begin;i<=9;i++)
-//         {
-//             combination.push_back(i);
-//             findcomb(target - i, ans, combination, i + 1, need );
-//             combination.pop_back();
-//         }
-//     }
-// };
