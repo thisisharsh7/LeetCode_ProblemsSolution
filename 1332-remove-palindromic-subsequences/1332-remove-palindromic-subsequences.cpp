347bbcb23449eee8912a92c4e17197569215f8ae
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-06-08 10:25:43 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return 2;
+            }
+            i++;
+            j--;
+            
+        }
+        return 1;
+    }
+};
