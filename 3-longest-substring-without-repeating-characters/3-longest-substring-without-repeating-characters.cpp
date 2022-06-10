75d212f6b6b94c51c5d9bad815304a81ba2e1116
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-06-10 06:21:20 +0530

    Time: 1401 ms (5.00%), Space: 149 MB (7.27%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length()==1){
+            return 1;
+        }
+        if(s.length()<1){
+            return 0;
+        }
+        unordered_map<char,int> freq;
+        int ans=0,cnt=0;
+        for(int i=0;i<s.length();i++){
+            freq[s[i]]++;
+            cnt++;
+           if(freq[s[i]]>1){
+                cnt=0;
+                i-=freq.size();// here we are moving our pointer back to the repeating character like in case of "pwwkew" we moved the i to the w so that we get a substring without repeating character
+                freq.clear();
+            }
+            if(cnt>ans){
+                ans=cnt;
+            }
+        }
+        return ans;
+        
+        
+        
+        
+    }
+};
