6575e15496543d12d360ebb4e44bd5c36e0027d7
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-06-27 22:13:29 +0530

    Time: 113 ms (11.05%), Space: 30.7 MB (6.15%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        
+        map<vector<int>,vector<string>> m;
+        
+        for(auto word : strs)
+        {
+            vector<int> cnt(26);      
+            for(auto ch : word){
+                cnt[ch - 'a'] += 1;  
+            } 
+            m[cnt].push_back(word);
+        }
+        for(auto it : m){
+            ans.push_back(it.second); 
+        }
+            
+        return ans;
+    }
+};
