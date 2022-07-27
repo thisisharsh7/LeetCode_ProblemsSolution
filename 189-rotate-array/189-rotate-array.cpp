d8c698a68ebe8b34bf432a1f9373159f5636d484
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-07-27 07:30:10 +0530

    Time: 56 ms (28.63%), Space: 24.9 MB (92.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int r=0;
         int n=nums.size();
-        // while(r<k){
-        //     // int x=nums[nums.size()-1];
-        //     // for(int i=nums.size()-2;i>=0;i--){
-        //     //     nums[i+1]=nums[i];
-        //     // }
-        //     // nums[0]=x;
-        //     r++;
-        // }
-        if(k>n){
-            k=k%n;//as if size is 3 and k is 4 then first and fourth rotation will be same
-            reverse(nums.begin(),nums.end());
-            reverse(nums.begin(),nums.begin()+k);
-            reverse(nums.begin()+k,nums.end());
-        }else{
-            reverse(nums.begin(),nums.end());
-            reverse(nums.begin(),nums.begin()+k);
-            reverse(nums.begin()+k,nums.end());
-        }
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin(),nums.begin()+k);
     }
 };
