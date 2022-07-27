04a94dcb458518b9fe4941555ecd4ca693583e6f
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-07-27 08:11:50 +0530

    Time: 3 ms (96.47%), Space: 5.9 MB (44.86%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -17,12 +17,21 @@ public:
     
     int countPrimeSetBits(int left, int right) {
         
-        int prime[1000001]={0};
-        primeSeive(prime);
+        // int prime[1000001]={0};
+        // primeSeive(prime);
+        // int cnt=0;
+        // for(int i=left;i<=right;i++){
+        //     int n=__builtin_popcount(i);
+        //     if(prime[n]){
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
         int cnt=0;
         for(int i=left;i<=right;i++){
             int n=__builtin_popcount(i);
-            if(prime[n]){
+            //As left and right are under 10 to the power 6 so the bits can not be more than 20 therefore we are taking condition only till 20 bits
+            if(n==2 || n==3 || n==5 || n==7 || n==11 || n==13 || n==17 || n==19){
                 cnt++;
             }
         }
