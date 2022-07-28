9cb48995dcb0ed5522a299cd66a7cf41754423cb
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-07-28 10:04:48 +0530

    Time: 12 ms (33.87%), Space: 10.8 MB (20.21%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -4,28 +4,31 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
         vector<int> ans;
-        unordered_map<int,int> m;
-        if(n1<n2){
-            for(int i=0;i<n1;i++){
-                m[nums1[i]]++;
-            }
-            for(int i=0;i<n2;i++){
-                if(m[nums2[i]]>=1){
-                    ans.push_back(nums2[i]);
-                    m[nums2[i]]--;
-                }
-            }
-        }else{
-            for(int i=0;i<n2;i++){
-                m[nums2[i]]++;
-            }
-            for(int i=0;i<n1;i++){
-                if(m[nums1[i]]>=1){
+        if(is_sorted(nums1.begin(),nums1.end()) and is_sorted(nums2.begin(),nums2.end())){
+            int i=0;
+            int j=0;
+            while(i<n1 and j<n2){
+                if(nums1[i]==nums2[j]){
                     ans.push_back(nums1[i]);
-                    m[nums1[i]]--;
+                    i++;
+                    j++;
+                }else if(nums1[i]<nums2[j]){
+                    i++;
+                }else{
+                    j++;
                 }
             }
         }
+        else{unordered_map<int,int> um;
+        for(int i=0;i<n1;i++){
+            um[nums1[i]]++;
+        }
+        for(int i=0;i<n2;i++){
+            if(um[nums2[i]]>0){
+                um[nums2[i]]--;
+                ans.push_back(nums2[i]);
+            }
+        }}
         return ans;
     }
 };
