9e91f303b35042d9f286483cc29cafc537c37cca
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-07-29 10:34:14 +0530

    Time: 28 ms (71.67%), Space: 23.3 MB (40.38%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    void reverse(vector<char> &s){
-        int i=0;
-        int j=s.size()-1;
-        while(i<=j){
-            swap(s[i],s[j]);
-            i++;
-            j--;
+    //two pointer
+    // void reverse(vector<char> &s){
+    //     int i=0;
+    //     int j=s.size()-1;
+    //     while(i<=j){
+    //         swap(s[i],s[j]);
+    //         i++;
+    //         j--;
+    //     }
+    //     return;
+    // }
+    //two pointer recursion
+    void reverse(vector<char> &v,int s,int e){
+        if(s>e){
+            return;
         }
-        return;
+        swap(v[s++],v[e--]);
+        reverse(v,s,e);
     }
     void reverseString(vector<char>& s) {
-        reverse(s);
+        reverse(s,0,s.size()-1);
     }
 };
