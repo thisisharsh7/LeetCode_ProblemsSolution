85a7752ea9708a2df0965363153f122ca5d35c9c
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-07-30 10:16:30 +0530

    Time: 15 ms (7.47%), Space: 10.6 MB (74.68%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,64 +10,32 @@
  */
 class Solution {
 public:
-    int length(ListNode*head){
-        int cnt=0;
-        while(head!=NULL){
-            cnt++;
-            head=head->next;
-        }
-        return cnt;
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next==NULL and n==1){
-            return NULL;
+        ListNode*length=head;
+        if(head==NULL){
+            return head;
         }
-        // int l=length(head);
-        // if(l==1 and n==1){
-        //     return NULL;
-        // }else if(l==1 and n>1){
-        //     return head;
-        // }
-        // else if(n==l){
-        //     ListNode*temp=head->next;
-        //     delete head;
-        //     head=temp;
-        //     return head;
-        // }
-        
-        // int p=l-n+1;
-        // ListNode* prev=NULL;
-        // ListNode* temp=head;
-        // while(p-1){
-        //     prev=temp;
-        //     temp=temp->next;
-        //     p--;
-        // }
-        // prev->next=temp->next;
-        // delete temp;
-        // return head;
-        ListNode*fast=head;
-        ListNode*slow=head;
-        for(int i=0;i<n;i++){
-            fast=fast->next;
+        int cnt=1;
+        while(length->next!=NULL){
+            length=length->next;
+            cnt++;
         }
-        ListNode*prev=NULL;
-        while(fast!=NULL){
-            fast=fast->next;
-            prev=slow;
-            slow=slow->next;
-           
-        }
-        if(prev==NULL){
+        cout<<cnt<<endl;
+        int reach=cnt-n;
+        if(reach==0){
             ListNode*temp=head->next;
             delete head;
             head=temp;
             return head;
         }
-        prev->next=slow->next;
-        delete slow;
-        
+        ListNode* prev=NULL;
+        ListNode* temp=head;
+        while(reach--){
+            prev=temp;
+            temp=temp->next;
+        }
+        prev->next=temp->next;
+        delete temp;
         return head;
-        
     }
 };
