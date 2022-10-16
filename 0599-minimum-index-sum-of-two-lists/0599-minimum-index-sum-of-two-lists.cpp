bb813e1084341a5e941d96784d2667b2274d4c9e
Harsh Kumar <9u.harsh@gmail.com>
2022-10-17 00:49:14 +0530

    Time: 378 ms (13.60%), Space: 30.2 MB (92.58%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -3,34 +3,21 @@ public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         int n1 = list1.size();
         vector<string> ans;
-        vector<int> num;
         int n2 = list2.size();
         int k = 0;
         int min = INT_MAX;
         for(int i = 0 ;i < n1;i++ ){
-            string s = list1[i];
             for(int j = 0;j < n2;j++){
-                if(list2[j]==s){
-                    
+                if(list2[j]==list1[i]){
                     k = i+j;
-                    
                     if(k <=min){
                          if(!ans.empty() && k!=min){
-                   
-                ans.pop_back();
-                    
+                            ans.pop_back();
+                         }
+                        ans.push_back(list1[i]); 
+                        min = k;
                 }
-                        ans.push_back(s); 
-                min = k;
-                        cout<<k<<endl;
-            }else{
-                if(!ans.empty()){
-                   ans.push_back(s); 
-                ans.pop_back();
-                    
-                }
-            }
-                    break;
+                 break;
                 }
             }
             
