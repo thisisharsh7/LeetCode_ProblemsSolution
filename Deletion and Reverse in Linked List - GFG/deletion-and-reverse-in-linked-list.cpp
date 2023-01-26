5fd14e03e89616c8efbceef9ec7e9f7a656752bd
Harsh Kumar <9u.harsh@gmail.com>
2023-01-26 22:17:51 +0530

    Added solution - LeetHub

---
+++ Deletion and Reverse in Linked List - GFG/deletion-and-reverse-in-linked-list.cpp
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+// C program to delete a given key from
+// linked list.
+#include<bits/stdc++.h>
+using namespace std;
+
+/* structure for a node */
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+
+Node *pre=NULL;
+
+/* Function to delete a given node from the list */
+//void deleteNode(struct Node *head, int key);
+void deleteNode(Node **head,int data);
+void reverse(Node **head);
+
+/* Function to reverse the linked list */
+//static void reverse(struct Node** head_ref);
+
+/* Function to insert a node at the beginning of
+a Circular linked list */
+
+void push(struct Node **head_ref, int data)
+{
+	// Create a new node and make head as next
+	// of it.
+
+        struct Node *curr = new Node(data);
+
+        curr->next=curr;
+    
+    	if(*head_ref==NULL)
+        {
+            *head_ref=curr;
+            curr->next=*head_ref;
+            pre=*head_ref;
+    
+        }
+        else
+        {
+            curr->next=*head_ref;
+            pre->next=curr;
+            pre=pre->next;
+    
+    
+        }
+}
+
+/* Function to print nodes in a given
+circular linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	if (head != NULL)
+	{
+		do
+		{
+			printf("%d ", temp->data);
+			temp = temp->next;
+		}
+		while (temp != head);
+	}
+    else
+    cout<<"empty"<<endl;
+
+	printf("\n");
+}
+/* Driver program to test above functions */
+int main()
+{
+
+	/* Initialize lists as empty */
+	int t,a,delnode;
+	cin>>t;
+	while(t--)
+	{
+        pre=NULL;
+	   Node *head=NULL;
+      
+        int n;
+	    cin>>n;
+	    while(n--)
+	    {
+	        cin>>a;
+	        push(&head,a);
+	    }    
+	/* Created linked list will be 2->5->7->8->10 */
+	cin>>delnode;
+
+	deleteNode(&head, delnode);
+	
+	reverse(&head);
+
+	printList(head);
+}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* structure for a node 
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+*/
+
+/* Function to delete a given node from the list */
+void deleteNode(struct Node **head, int key)
+{
+
+// Your code goes here
+Node*temp = *head;
+if(temp==NULL){
+    return;
+}
+while(temp!=NULL){
+    if(temp->next->data==key){
+        temp->next = temp->next->next;
+        return;
+    }
+    temp = temp->next;
+}
+
+}
+
+/* Function to reverse the linked list */
+ void reverse(struct Node** head_ref)
+{
+    Node* head = *head_ref;
+    Node* curr = head->next;
+    Node* prev = head;
+    Node* n ;
+    while(curr != head){
+        n = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = n;
+    }
+    curr->next = prev;
+    *head_ref = prev;
+    return;
+
+// Your code goes here
+
+
+}
