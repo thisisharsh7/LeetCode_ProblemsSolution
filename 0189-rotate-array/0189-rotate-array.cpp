14a5442cd6aff69103da34c9bd7670038f36430e
Harsh Kumar <9u.harsh@gmail.com>
2023-10-03 17:35:00 +0530

    Time: 24 ms (41.02%), Space: 25.1 MB (67.17%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,7 +4,8 @@ public:
         int n = nums.size();
         k = k%n;
         reverse(nums.begin(),nums.end());
-        reverse(nums.begin()+k,nums.end());
         reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        return;
     }
 };
